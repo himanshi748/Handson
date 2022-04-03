364f2de4d6fe8cb3aaa6e92c137b9d188e6c40ed
Himanshi Satpute <57482731+himanshi748@users.noreply.github.com>
2022-04-03 14:30:32 +0530

    cookie

---
+++ cookies.js
@@ -0,0 +1,16 @@
+// cookie
+
+// document.cookie = 'user = aditi';
+// document.cookie = 'id  = 44';
+// document.cookie = 'admin = true';
+// console.log(document.cookie);
+
+// document.cookie = 'name = rahul ;max-age = 1';
+
+// document.cookie = 'admin = true ; max-age = -1';
+
+// // session storage
+
+// localStorage.setItem('greet','Hi')
+// localStorage.setItem(key,value);
+// console.log(localStorage.getItem('greet'));
